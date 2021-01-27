def0c0fd410d50cc0ce000b6caaa8e2e248c41a9
jBialas <julian.bialas@gmail.com>
2021-01-27 09:10:57 +0100

    add title picture

--- source/index.blade.php
+++ source/index.blade.php
@@ -8,5 +8,7 @@
     <p>Speziell habe ich mich damit befasst, wie man das FH eigene JupyterHub verbessern kann.</p>
 
     <p>Sie finden die Blogs über GPU sharing und JupyterLab extensions in dem Posts panel.</p>
+    
+    <img src="https://cdn.app.compendium.com/uploads/user/e7c690e8-6ff9-102a-ac6d-e4aebca50425/a0e1a9f4-b2f2-496b-ac14-50032102b3f3/File/97c92c0895a7128c241a73a998d81130/big_data_virtual_grid.jpg" alt="the world of data">
 
 @endsection
