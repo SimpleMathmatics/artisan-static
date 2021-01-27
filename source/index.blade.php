f653ec321bf99202ac4723b189f485ce1e6181b8
jBialas <julian.bialas@gmail.com>
2021-01-27 16:04:43 +0100

    added secound blog

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,12 +3,12 @@
 @section('content')
     <h1>Willkommen auf meinem Big Data Processing Blog</h1>
 
+    <img src="https://cdn.app.compendium.com/uploads/user/e7c690e8-6ff9-102a-ac6d-e4aebca50425/a0e1a9f4-b2f2-496b-ac14-50032102b3f3/File/97c92c0895a7128c241a73a998d81130/big_data_virtual_grid.jpg" alt="the world of data">
+
     <p>Im Rahmen des Kurses Big Data Processing an der FH Kufstein habe ich, Julian Bialas einen Blog eröffnet.</p>
 
     <p>Speziell habe ich mich damit befasst, wie man das FH eigene JupyterHub verbessern kann.</p>
 
     <p>Sie finden die Blogs über GPU sharing und JupyterLab extensions in dem Posts panel.</p>
     
-    <img src="https://cdn.app.compendium.com/uploads/user/e7c690e8-6ff9-102a-ac6d-e4aebca50425/a0e1a9f4-b2f2-496b-ac14-50032102b3f3/File/97c92c0895a7128c241a73a998d81130/big_data_virtual_grid.jpg" alt="the world of data">
-
 @endsection
